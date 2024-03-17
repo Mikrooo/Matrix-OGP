3262cb2186800e6a628d24ae2738208a05d8d084
Mirko Naessens <mirko.naessens@student.kuleuven.be>
2024-03-17 12:36:25 +0100

    immutable API

--- Matrix/src/matrix/Matrix.java
+++ Matrix/src/matrix/Matrix.java
@@ -1,98 +1,98 @@
 package matrix;
 
+import java.util.stream.IntStream;
+
 import java.util.Arrays;
 
 /**
- * in deze class kunnen we matrices ingeven in elke instantie
- * geen setters omdat de class immutable is
+ * elke instantie van deze klasse stelt een matrix an double-waarden met een gegeven hoogte en breedte voor
+ * @invar | 0 <= getAantalRijen()
+ * @invar | 0 <= getAantalKolommen()
+ * 
+ * @immutable
  */
 
 public class Matrix {
-	
-	
-	/**
-	 * 
-	 * @invar de array elements kan niet nul zijn
-	 * 	| elements != null
-	 * @invar rows kan niet negatief zijn
-	 * 	| rows > 0
-	 * @invar columns kunnen niet negatief of nul zijn
-	 * 	| columns > 0
-	 * @invar amount of elements in de array elements is gelijk aan het aantal rows maal het aantal columns
-	 * 	| elements.length == rows + columns - 1
-	 * 
-	 */
-	private double[] elements;
-	private int rows;
-	private int columns;
-	
-	// constructor
-	
-	/**
-	 * @pre initElements is initialized
-	 * 	| initElements != null
-	 * @pre initRows is positief
-	 * 	| initRows > 0
-	 * @pre initcolumns is positief
-	 * 	| initColumns > 0
-	 * 
-	 * @post | initElements.equals(rowMajorOrder()) || initElements.equals(columnMajorOrder())
-	 * @post | initRows == getAmountRows()
-	 * @post | initColumns == getAmountColumns()
-	 * 
-	 */
-	public Matrix(double[] initElements, int initRows, int initColumns) {
-		elements = initElements; //this should be copied due to a certain exposure, but it can be skipped in this program bc the elements can't be changed
-		rows = initRows;
-		columns = initColumns;
+
+
+	public int getAantalRijen() {
+		throw new RuntimeException("Not yet implemented");
+		
 	}
-	
-	
-	// getters
-	
-	
-	
-	public int getAmountRows() {
-		return rows;
-	}
-	
-	public int getAmountColumns() {
-		return columns;
+
+	public int getAantalKolommen() {
+		throw new RuntimeException("Not yet implemented");
+
 	}
 	
 	
 	/**
-	 * @pre | row <= getAmountRows()
-	 * @pre | column <= getAmountColumns()
+	 * @pre | 0 <= rijIndex && rijIndex < getAantalRijen()
+	 * @pre | 0 <= kolomIndex && kolomIndex < getAantalRijen()
+	 * @post | result == getElementen()[rijIndex][kolomIndex]
+	 */
+	public double getElementAt(int rijIndex, int kolomIndex) {
+		throw new RuntimeException("Not yet implemented");
+
+	}
+	
+	/**
+	 * @creates | result
+	 * @post | result != null
+	 * @post | result.length == getAantalRijen() * getAantalKolommen()
+	 * @post | IntStream.range(0, getAantalRijen()).allMatch(rijIndex ->
+	 * 	     | 	 IntStream.range(0, getAantalKolommen()).allMatch(kolomIndex ->
+	 * 		 |		result[rijIndex * getAantalKolommen() + kolomIndex] == getElementen()[rijIndex][kolomIndex]
+	 * 		 |	 )
+	 * 		 | )
+	 */
+	public double[] getElementenRowMajor() {
+		throw new RuntimeException("Not yet implemented");
+
+	}
+	
+	/**
+	 * @creates | result
+	 * @post | result != null
+	 * @post | result.length == getAantalRijen() * getAantalKolommen()
+	 * @post | IntStream.range(0, getAantalRijen()).allMatch(rijIndex ->
+	 * 	     | 	 IntStream.range(0, getAantalKolommen()).allMatch(kolomIndex ->
+	 * 		 |		result[kolomIndex * getAantalRijen() + rijIndex] == getElementen()[rijIndex][kolomIndex]
+	 * 		 |	 )
+	 * 		 | )
+	 */
+	public double[] getElementenColumnMajor() {
+		throw new RuntimeException("Not yet implemented");
+	}
+	
+	
+	/**
+	 * @creates De array alsook haar elementen zijn nieuw gecreeerd tijdens deze oproep van getElementen().
+	 *  | result, ...result
+	 * @post | result != null
+	 * @post | !Arrays.stream(result).allMatch(rij -> rij != null && rij.length == getAantalKolommen())
+	 */
+	public double[][] getElementen() {
+		throw new RuntimeException("Not yet implemented");
+
+	}
+	
+	/**
+	 * @pre | 0 <= aantalRijen
+	 * @pre | 0 <= aantalKolommen
+	 * @pre | elementenRowMajor != null
+	 * @pre | elementenRowMajor.length == aantalRijen * aantalKolommen
+	 * 
+	 * @inspects
+	 * 
+	 * @post | getAantalRijen() == aantalRijen
+	 * @post | getAantalKolommen() == aantalKolommen
+	 * @post | Arrays.equals(getElementenRowMajor(), elementenRowMajor)
 	 * 
 	 */
-	public double getElementAt(int row, int column) {
-		return elements[row * 3 + column];
+	public Matrix(int aantalRijen, int aantalKolommen, double[] elementenRowMajor) {
+		throw new RuntimeException("Not yet implemented");
 	}
 	
-	public double[] rowMajorOrder() {
-		return elements;
-	}
 	
-	public double[] columnMajorOrder() {
-		double[] newArray = new double[elements.length];
-		for (int col = 0; col < columns; col++) {
-			for (int row = 0; row < columns; row++) {
-				newArray[col * row + row] = elements[row * col + col];
-			}
-		}
-		return newArray;
-	}
-	
-	public double[] arrayOfRows() {
-		double[][] newArray = new double[rows][columns];
-		for (int col = 0; col < columns; col++) {
-			for (int row = 0; row < rows; row++) {
-			
-				newArray[row][col] = elements[row * col + col];
-			}
-			
-		}
-		return elements;
-	}
 }
