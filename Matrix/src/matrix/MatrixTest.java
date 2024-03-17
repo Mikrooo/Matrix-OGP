3785ceeddca824637429571388fb29ad551651b4
Mirko Naessens <mirko.naessens@student.kuleuven.be>
2024-03-17 13:55:53 +0100

    Immutable API en test Suite

--- Matrix/src/matrix/MatrixTest.java
+++ Matrix/src/matrix/MatrixTest.java
@@ -1,5 +1,21 @@
 package matrix;
 
-public class MatrixTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MatrixTest {
+
+	@Test
+	void test() {
+		double[] elementenRowMajor = {10, 20, 30, 40, 50, 60};
+		Matrix myMatrix = new Matrix(2, 3, elementenRowMajor);
+		assertEquals(2, myMatrix.getAantalRijen());
+		assertEquals(3, myMatrix.getAantalKolommen());
+		assertArrayEquals(new double[] {10, 20, 30, 40, 50, 60}, myMatrix.getElementenRowMajor());
+		assertArrayEquals(new double[] {10, 40, 20, 50, 30, 60}, myMatrix.getElementenColumnMajor());
+
+		assertArrayEquals(new double[][] {{10, 20, 30}, {40, 50, 60}}, myMatrix.getElementen());
+	}
 
 }
